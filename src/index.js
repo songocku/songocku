2e2cad62e4ad78f72b40722edf08b669fd9845d4
Marius <marius_zerg@yahoo.com>
2019-08-29 23:19:11 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+const Hello = (props) => {
+	return (
+	
+	<div>
+		<p>Hello {props.name}</p>
+	</div>
+	)
+
+}
+
+
+
+const App = () =>{
+	
+	const now = new Date()
+	const a = 10
+	const b = 20
+	
+	
+return (
+	<div>
+		<p>Hello World, it is {now.toString()}</p>
+		<p>{a} + {b} = {a+b}</p>
+		
+		<h1>Greeting</h1>
+		<Hello name = "marius" />
+		<Hello name = "prodan" />
+		
+		
+		</div>
+)
+}
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
